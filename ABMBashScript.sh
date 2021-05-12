6056e1fd07badd90507aab2b6b02c228863929e4
ricardoi <ralcala@ufl.edu>
2021-05-12 12:13:02 -0400

    high perfomance computing

--- ABMBashScript.sh
+++ ABMBashScript.sh
@@ -10,11 +10,14 @@
 #SBATCH --mem=30gb                   # Memory limit
 #SBATCH --time=20:00:00               # Time limit hrs:min:sec
 #SBATCH --output=ABM.Run2%j.out   # Standard output and error log
+#SBATCH --array=1-100:1
 
 pwd;hostname;date
 
 module load ufrc
 module load R/3.6
+
+ARGS=$(sed -n {SLURM_ARRAY_TASK_ID}p ParameterTable.txt)
 
 Rscript --save /blue/garrett/betherton/LW2020/forHPG.R
 date
