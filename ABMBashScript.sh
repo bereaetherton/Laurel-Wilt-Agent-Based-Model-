a36ffb1a11de8a35126afda3603cb41d2bec18e9
bereaetherton <betherton@ufl.edu>
2021-05-27 16:17:26 -0400

    Bash Change:

--- ABMBashScript.sh
+++ ABMBashScript.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1                     # Use one node
 #SBATCH --ntasks=1                    # Run a single task
 #SBATCH --cpus-per-task=12             # Use 1 core
-#SBATCH --mem=8gb                   # Memory limit
+#SBATCH --mem=1gb                   # Memory limit
 #SBATCH --time=150:00:00               # Time limit hrs:min:sec
 #SBATCH --output=ABM.Run2%j.out   # Standard output and error log
 #SBATCH --array=1-14:1
